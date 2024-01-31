21ab6dca87de9f7658fd8c437df3ec6674c5ad58
bryan-pereyra <122488694+bryan-pereyra@users.noreply.github.com>
2024-01-31 16:01:12 -0600

    cy.title() and its validation

--- cypress/e2e/3-visit.cy.ts
+++ cypress/e2e/3-visit.cy.ts
@@ -10,4 +10,11 @@ describe('basics', () => {
             expect(url).to.contains("/textinput");
         });
     });
+
+    it("Title validation", () => {
+        cy.title().then((title) => {
+            cy.log(title);
+            expect(title).to.be.equal("Text Input");
+        });
+    });
 });

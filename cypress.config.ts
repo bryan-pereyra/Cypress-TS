9400b44e9ca52c45dc94735359a52b2c203ef34b
bryan-pereyra <122488694+bryan-pereyra@users.noreply.github.com>
2024-02-07 11:43:41 -0600

    Environment Variables exercise

--- cypress.config.ts
+++ cypress.config.ts
@@ -6,5 +6,11 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    env: {
+      demoVar: "Hello from the cypress.config.ts file",
+      demoQA: "https://demoqa.com/",
+      theInternet: "https://the-internet.herokuapp.com/",
+      Angular: "https://www.globalsqa.com/",
+    },
   },
 });

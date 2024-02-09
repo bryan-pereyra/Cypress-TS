0fcefdfe288e6566802bac43a6ad2f0df2a9f3c9
bryan-pereyra <122488694+bryan-pereyra@users.noreply.github.com>
2024-02-09 12:51:13 -0600

    Links exercises

---
+++ cypress/e2e/12-links.cy.ts
@@ -0,0 +1,35 @@
+describe('Dealing with links that opens a new tab', () => {
+
+    beforeEach(() => {
+        cy.visit(`${Cypress.env('demoQA')}/links`);
+    });
+
+    it('First approach, not click on the link', () => {
+        cy.get('a#simpleLink').should('have.attr', 'href', 'https://demoqa.com');
+        cy.get('a#simpleLink').should('have.attr', 'target', '_blank');
+    });
+
+    it('Second approach, remove the target', () => {
+        cy.get('a#simpleLink').invoke('removeAttr', 'target').click();
+        cy.url().then((url) => {
+            expect(url).to.be.equal('https://demoqa.com/');
+        });
+    });
+});
+
+describe('Intercepting API (Spying) request after clicking on a button', () => {
+
+    beforeEach(() => {
+        cy.visit(`${Cypress.env('demoQA')}/links`);
+        cy.intercept('GET', `${Cypress.env('demoQA')}/created`).as('linkStatus');
+    });
+
+    it('Using cy.intercept()', () => {
+        cy.get('a#created').click();
+        cy.wait('@linkStatus').then((request) => {
+            cy.log('This is the request intercepted', request);
+            expect(request.response.statusCode).to.be.equal(201);
+            expect(request.response.statusMessage).to.be.equal('Created');
+        });
+    });
+});
